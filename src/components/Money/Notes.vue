68c3cd4d3ad0189b258163da91002f4e2fbe54a6
Adin <1502842832@qq.com>
2021-08-18 13:23:03 +0800

    可以选择或取消tag

--- src/components/Money/Notes.vue
+++ src/components/Money/Notes.vue
@@ -15,7 +15,6 @@ import {Component} from 'vue-property-decorator';
 @Component
 export default class Notes extends Vue {
   value = '';
-
 }
 </script>
 
--- src/views/Money.vue
+++ src/views/Money.vue
@@ -3,25 +3,31 @@
     <NumberPad/>
     <Types/>
     <Notes/>
-    <Tags/>
+    <Tags :data-source="tags"/>
   </Layout>
 </template>
 
-<script lang="ts">
+<script>
 
-import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
-import Notes from '@/components/Money/Notes.vue';
-import Tags from '@/components/Money/Tags.vue';
+import NumberPad from '@/components/Money/NumberPad.vue'
+import Types from '@/components/Money/Types.vue'
+import Notes from '@/components/Money/Notes.vue'
+import Tags from '@/components/Money/Tags.vue'
+
 export default {
   name: 'Money',
   components: {Tags, Notes, Types, NumberPad},
-};
+  data() {
+    return {
+      tags: ['衣', '食', '住', '行','游戏']
+    }
+  }
+}
 </script>
 
 <style lang="scss">
-.layout-content{
-  display:flex;
+.layout-content {
+  display: flex;
   flex-direction: column-reverse;
 }
 </style>
