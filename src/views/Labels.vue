076a576435716defa8f32fb8076bfc002d6ca11b
Adin <1502842832@qq.com>
2021-08-21 12:48:09 +0800

    labels页面的html和css

--- src/views/Labels.vue
+++ src/views/Labels.vue
@@ -1,7 +1,27 @@
 <template>
-    <Layout>
-      <p>Labels.vue</p>
-    </Layout>
+  <Layout>
+    <ol class="tags">
+      <li>
+        <span>衣</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>食</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>住</span>
+        <Icon name="right"/>
+      </li>
+      <li>
+        <span>行</span>
+        <Icon name="right"/>
+      </li>
+    </ol>
+    <div class="createTag-wrapper">
+      <button class="createTag">新建标签</button>
+    </div>
+  </Layout>
 </template>
 
 <script lang="ts">
@@ -10,3 +30,38 @@ export default {
   name: 'Labels',
 };
 </script>
+
+<style lang="scss" scoped>
+.tags {
+  background: white;
+  font-size: 16px;
+  padding-left: 16px;
+
+  > li {
+    min-height: 44px;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    border-bottom: 1px solid #e6e6e6;
+    svg{
+      width: 20px;
+      height: 20px;
+      color: #666;
+      margin-right: 16px;
+    }
+  }
+}
+.createTag{
+  background: #666666;
+  color: white;
+  border-radius: 4px;
+  border: none;
+  height: 40px;
+  padding: 0 16px;
+  &-wrapper{
+    text-align: center;
+    padding: 16px;
+    margin-top: 44-16px;
+  }
+}
+</style>
