4a043f93c67c9c5d8f932a8da5e3df4f1efa34ad
Adin <1502842832@qq.com>
2021-08-31 22:20:37 +0800

    全局使用Vuex落地

--- src/views/EditLabel.vue
+++ src/views/EditLabel.vue
@@ -3,10 +3,10 @@
     <div class="navBar">
       <Icon class="leftIcon" name="left" @click="goBack"/>
       <span class="title">编辑标签</span>
-      <span class="rightIcon"></span>
+      <span class="rightIcon" />
     </div>
     <div class="form-wrapper">
-      <FormItem :value="tag.name"
+      <FormItem :value="currentTag.name"
                 @update:value="update"
                 field-name="标签名" placeholder="请输入标签名"/>
     </div>
@@ -27,34 +27,28 @@ import Button from '@/components/Button.vue';
 })
 export default class EditLabel extends Vue {
   // eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
-  get tag() {
+  get currentTag() {
     return this.$store.state.currentTag;
   }
 
   created() {
     const id = this.$route.params.id;
+    this.$store.commit('fetchTags')
     this.$store.commit('setCurrentTag', id);
-    if (!this.tag) {
+    if (!this.currentTag) {
       this.$router.replace('/404');
     }
   }
 
   update(name: string) {
-    if (this.tag) {
-      // todo
-      // store.updateTag(this.tag.id, name);
+    if (this.currentTag) {
+      this.$store.commit('updateTag',{id:this.currentTag.id,name:name})
     }
   }
 
   remove() {
-    if (this.tag) {
-      return;
-      // todo
-      // if (store.removeTag(this.tag.id)) {
-      //   this.$router.back();
-      // } else {
-      //   window.alert('删除失败');
-      // }
+    if (this.currentTag) {
+      this.$store.commit('removeTag',this.currentTag.id)
     }
   }
 
