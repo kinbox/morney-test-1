97edb4aa98c77f0710ad24cd38f6e739a9ce37d2
Adin <1502842832@qq.com>
2021-09-01 12:13:06 +0800

    在Money.vue 里面使用 Tabs

--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -1,6 +1,6 @@
 <template>
   <Layout>
-    <Tabs class-prefix="type" :data-source="typeList" :value.sync="type"/>
+    <Tabs class-prefix="type" :data-source="recordTypeList" :value.sync="type"/>
     <Tabs class-prefix="interval" :data-source="intervalList" :value.sync="interval"/>
     <div>
     type:{{type}}
@@ -31,6 +31,9 @@ import Types from '@/components/Money/Types.vue';
 import Vue from 'vue';
 import {Component} from 'vue-property-decorator';
 import Tabs from '@/components/Tabs.vue';
+import intervalList from '@/constant/intervalList';
+import recordTypeList from '@/constant/recordTypeList'
+
 
 @Component({
   components: {Tabs, Types},
@@ -38,14 +41,7 @@ import Tabs from '@/components/Tabs.vue';
 export default class Statistics extends Vue {
   type = '-';
   interval = 'day';
-  intervalList = [
-    {text: '按天', value: 'day'},
-    {text: '按周', value: 'week'},
-    {text: '按月', value: 'month'},
-  ];
-  typeList = [
-    {text: '支出', value: '-'},
-    {text: '收入', value: '+'}
-  ];
+  intervalList = intervalList
+  recordTypeList = recordTypeList
 }
 </script>
